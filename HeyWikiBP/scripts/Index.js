099e103102a1e23e041e98f2fa739f927f8581b3
MetalManeMc <metalmaniac1331@outlook.com>
2024-04-07 10:34:27 +0200

    /wiki base. Will start work on UI now.

--- HeyWikiBP/scripts/Index.js
+++ HeyWikiBP/scripts/Index.js
@@ -1,5 +1,11 @@
+// In-game debug command: /script debugger connect localhost 19144
+
 import { world } from "@minecraft/server"
 
 world.beforeEvents.chatSend.subscribe((eventitem) => {
-    const {message, sender} = eventitem
+    const { message, sender } = eventitem
+    if (message.startsWith("!wiki ")) {
+        eventitem.cancel = true
+        sender.sendMessage("https://minecraft.wiki/w/?search=" + message.substr(6))
+    }
 })
